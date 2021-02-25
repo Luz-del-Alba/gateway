09848ba9ce541ad7fd6a1f56aa23860a1ed17ae9
Cristhian Castaneda <ccastanedarivera@gmail.com>
2021-02-25 01:44:24 -0600

    Add CI/CD

--- src/main/java/com/ust/gateway/GatewayApplication.java
+++ src/main/java/com/ust/gateway/GatewayApplication.java
@@ -1,13 +1,25 @@
 package com.ust.gateway;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("backend", r -> r.path("/get")
+                        .uri("http://localhost"))
+                .build();
+    }
+
 
 }
